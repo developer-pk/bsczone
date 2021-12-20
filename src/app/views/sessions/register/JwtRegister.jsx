1a4cb66926d1859ebdecd1295a04b9b7465bf403
subhashthakur37 <subhash_thakur@esferasoft.com>
2021-12-20 14:24:24 +0530

    css update LOGIN REGISETR FORGOT

--- src/app/views/sessions/register/JwtRegister.jsx
+++ src/app/views/sessions/register/JwtRegister.jsx
@@ -64,17 +64,10 @@ const JwtRegister = () => {
         >
             <Card className={classes.card}>
                 <Grid container>
-                    <Grid item lg={5} md={5} sm={5} xs={12}>
-                        <div className="p-8 flex justify-center bg-light-gray items-center h-full">
-                            <img
-                                className="w-full"
-                                src="/assets/images/illustrations/posting_photo.svg"
-                                alt=""
-                            />
-                        </div>
-                    </Grid>
-                    <Grid item lg={7} md={7} sm={7} xs={12}>
-                        <div className="p-8 h-full">
+                    
+                    <Grid item>
+                        <div className="login_form">
+                         <h4>REGISTER</h4>
                             <ValidatorForm onSubmit={handleFormSubmit}>
                                 <TextValidator
                                     className="mb-6 w-full"
@@ -147,8 +140,8 @@ const JwtRegister = () => {
                                     }
                                     label="I have read and agree to the terms of service."
                                 />
-                                <div className="flex items-center">
-                                    <Button
+                                <div className="flex items-center buton-css">
+                                    <Button className="signin"
                                         className="capitalize"
                                         variant="contained"
                                         color="primary"
